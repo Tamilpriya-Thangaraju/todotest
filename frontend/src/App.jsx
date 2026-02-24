461baef7443d2dfa29a276593ab0663560609ee2
Tamilpriya-Thangaraju <tamilpriya.ts@pwc.com>
2026-02-24 05:21:32 -0500

    Resolved a case sensitivity mismatch to build docker image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Header from './components/Header';
+import Header from './components/header';
 import StatsCards from './components/StatsCards';
 import AddTaskForm from './components/AddTaskForm';
 import TaskList from './components/TaskList';
